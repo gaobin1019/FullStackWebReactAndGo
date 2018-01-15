e0fb87319c04a2121609a357fcec541a745b26f5
Bin Gao <gaobin1019@gmail.com>
2018-01-14 17:49:51 -0800

    enabled cross origin

--- service/main.go
+++ service/main.go
@@ -69,14 +69,14 @@ func main() {
 	r := mux.NewRouter()
 
 	var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
-		ValidationKeyGetter: func(token *jwt.Token)(interface{}, error) {
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
 			return privateKey, nil
 		},
 		SigningMethod: jwt.SigningMethodHS256,
 	})
 
-	r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST")
-	r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET")
+	r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost)))
+	r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch)))
 	r.Handle("/login", http.HandlerFunc(handlerLogin)).Methods("POST")
 	r.Handle("/signup", http.HandlerFunc(handlerSignUp)).Methods("POST")
 
@@ -88,6 +88,11 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+	if r.Method != "POST" {
+		return
+	}
+
 	//32MB
 	r.ParseMultipartForm(32 << 20)
 
@@ -137,87 +142,16 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	//saveToBT(&p, id)
 }
 
-func saveToGCS(ctx context.Context, file io.Reader, bucket, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error){
-	//create a client
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		log.Fatalf("***Failed to create client: %v", err)
-	}
-	defer client.Close()
-
-	bh := client.Bucket(bucket)
-	//check if the bucket exists
-	if _, err = bh.Attrs(ctx); err != nil {
-		return nil, nil, err
-	}
-
-	obj := bh.Object(name)
-	w := obj.NewWriter(ctx)
-	if _, err := io.Copy(w, file); err != nil {
-		return nil, nil, err
-	}
-	if err := w.Close(); err != nil {
-		return nil, nil, err
-	}
-
-	//set access control to public read
-	if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
-		return nil, nil, err
-	}
-
-	attrs, err := obj.Attrs(ctx)
-	fmt.Printf("***Post is saved to GCS: %s\n", attrs.MediaLink)
-	return obj, attrs, err
-}
-
-//save to google bigtable, VERY EXPANSIVE!!! disabled for now
-/*
-func saveToBT(p *Post, id string) {
-	ctx := context.Background()
-	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
-	if err != nil {
-		panic(err)
-		return
-	}
-
-	tbl := bt_client.Open("post")
-	mut := bigtable.NewMutation()
-	mut.Set("post", "user", bigtable.Now(), []byte(p.User))
-	mut.Set("post", "message", bigtable.Now(), []byte(p.Message))
-
-	mut.Set("location", "lat", bigtable.Now(), []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
-	mut.Set("location", "lon", bigtable.Now(), []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
-
-	err = tbl.Apply(ctx, id, mut)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
-}
-*/
-
-func saveToES(p *Post, id string) {
-	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
-	if err != nil {
-		panic(err)
-		return
-	}
-
-	_, err = client.
-		Index().
-		Index(INDEX).
-		Type(TYPE).
-		Id(id).
-		BodyJson(p).
-		Refresh(true).
-		Do()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("***Post is saved to elasticSearch: %s\n", p.Message)
-}
-
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+	if r.Method != "GET" {
+		return
+	}
+
 	fmt.Println("***Received a search request")
 	lat, err := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
 	if err != nil {
@@ -235,8 +169,6 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	if val := r.URL.Query().Get("range"); val != "" {
 		ran = val + "km"
 	}
-
-	fmt.Fprintf(w, "search received: %v %v %v\n", lat, lon, ran)
 
 	//search on redis first
 	key := r.URL.Query().Get("lat") + ":" + r.URL.Query().Get("lon") + ":" + ran
@@ -315,8 +247,6 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
 }
 
@@ -333,3 +263,83 @@ func containsFilteredWords(s *string) bool {
 
 	return false
 }
+
+func saveToES(p *Post, id string) {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	_, err = client.
+		Index().
+		Index(INDEX).
+		Type(TYPE).
+		Id(id).
+		BodyJson(p).
+		Refresh(true).
+		Do()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("***Post is saved to elasticSearch: %s\n", p.Message)
+}
+
+func saveToGCS(ctx context.Context, file io.Reader, bucket, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error){
+	//create a client
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("***Failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	bh := client.Bucket(bucket)
+	//check if the bucket exists
+	if _, err = bh.Attrs(ctx); err != nil {
+		return nil, nil, err
+	}
+
+	obj := bh.Object(name)
+	w := obj.NewWriter(ctx)
+	if _, err := io.Copy(w, file); err != nil {
+		return nil, nil, err
+	}
+	if err := w.Close(); err != nil {
+		return nil, nil, err
+	}
+
+	//set access control to public read
+	if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, nil, err
+	}
+
+	attrs, err := obj.Attrs(ctx)
+	fmt.Printf("***Post is saved to GCS: %s\n", attrs.MediaLink)
+	return obj, attrs, err
+}
+
+//save to google bigtable, VERY EXPANSIVE!!! disabled for now
+/*
+func saveToBT(p *Post, id string) {
+	ctx := context.Background()
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	mut.Set("post", "user", bigtable.Now(), []byte(p.User))
+	mut.Set("post", "message", bigtable.Now(), []byte(p.Message))
+
+	mut.Set("location", "lat", bigtable.Now(), []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", bigtable.Now(), []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
+}
+*/
